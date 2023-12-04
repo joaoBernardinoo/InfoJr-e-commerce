b490df53f439832c13a0cc5e378f06e45e36d77f
Lucas Júlio de Souza <lucasjuliosouza.06@gmail.com>
2023-12-04 16:51:28 -0300

    refact: tipo para criar produto

--- src/types/products.d.ts
+++ src/types/products.d.ts
@@ -1,3 +1,9 @@
 export interface Product {
-    
+    name: string,
+    current_price: float,
+    old_price: float,
+    available_quantity: int,
+    size: string,
+    product_description: string,
+    Tags: string
 }
