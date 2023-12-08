cd1f436725d82e8c2e2c90201ecb179fb61dbcc5
joaoBernardinoo <joaobernardino@ufba.br>
2023-12-07 23:17:02 -0300

    cards adicionado

--- src/types/products.d.ts
+++ src/types/products.d.ts
@@ -1,11 +1,11 @@
-export interface Product {
+export type Product = {
     name: string,
     current_price: float,
     old_price: float,
     available_quantity: int,
     size: string,
     product_description: string,
-    Tags: string
+    tags: string
 }
 
 export type ProductResponse = {
