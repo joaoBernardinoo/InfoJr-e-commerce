06b2913dc96e05d8bc247954d65648e8e37d808c
Lucas Júlio de Souza <lucasjuliosouza.06@gmail.com>
2023-12-08 23:28:41 -0300

    algumas funções do carrinho

--- src/app/utils/functions.ts
+++ src/app/utils/functions.ts
@@ -19,3 +19,18 @@ export function recentProducts(recentProducts: Product[], prod: Product){
     recentProducts.shift()
     recentProducts.push(prod)
 }
+
+// Carrinho
+
+export function addCart(prodsCart: Product[], prodAdd: Product) {
+    prodsCart.push(prodAdd)
+}
+
+export function removeCart(prodsCart: Product[], prodId: number){
+    prodsCart = prodsCart.filter((prodCart: Product) => prodCart.id != prodId)
+}
+
+export function clearCart(prodsCart: Product[]){
+    prodsCart
+}
+
