2598d68b3a013028b15c3e0ab2b9d414125096d5
TCosta <tcostasouza0@gmail.com>
2023-12-09 06:30:46 -0300

    style: Finalizando a estilização da busca na página de produtos

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 import { useState, useContext, useEffect } from 'react';
 import produtos from '../imagens/cards_produtos/card1.png';
 import filter from '../imagens/filtro/filter_list.png';
@@ -9,21 +9,21 @@ import Link from 'next/link';
 import ProductContext from '@/contexts/products';
 import { filterProducts, otherProducts } from '@/utils/functions';
 import { searchProducts } from '@/utils/functions';
+import { FaSearch } from "react-icons/fa";
 
 export default function Produtos() {
-
   const { products } = useContext(ProductContext);
 
-  const [prodsFilter, setProdsFilter] = useState(products)
-  const [word, setWord] = useState("")
+  const [prodsFilter, setProdsFilter] = useState(products);
+  const [word, setWord] = useState('');
 
   useEffect(() => {
     handleSearch();
   }, [word]); // Observa mudanças no estado 'word'
 
   function handleSearch() {
-      const filtered = word ? searchProducts(products, word) : products;
-      setProdsFilter(filtered);
+    const filtered = word ? searchProducts(products, word) : products;
+    setProdsFilter(filtered);
   }
 
   return (
@@ -32,7 +32,13 @@ export default function Produtos() {
         <div className={styles.titulo}>
           <h1>Produtos</h1>
           <div className={styles.prod_barra}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="257" height="14" viewBox="0 0 257 14" fill="none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="257"
+              height="14"
+              viewBox="0 0 257 14"
+              fill="none"
+            >
               <path d="M0 0H257V14H0V0Z" fill="#D1D1D1" />
             </svg>
           </div>
@@ -40,22 +46,26 @@ export default function Produtos() {
         <div className={styles.box2}>
           <div className={styles.box3}>
             <div className={styles.l_esq}>
-              <input onChange={(e) => setWord((e.target.value))}
-                className={styles.pesquisar}
-                value={word}
-                placeholder="Pesquisar"
-              ></input>
-              {/*Tentar atualizar o filtro com o onChange no input*/}
-              <button onClick={() => handleSearch()}>Pesquisar</button>
+              <div className={styles.barraPesquisa}>
+                <input
+                  onChange={e => setWord(e.target.value)}
+                  className={styles.campoPesquisa}
+                  value={word}
+                  placeholder="Pesquisar"
+                ></input>
+                {/*Tentar atualizar o filtro com o onChange no input*/}
+                <button className={styles.botaoBusca} onClick={() => handleSearch()}><FaSearch /></button>
+              </div>
               <h3>X itens encontrados</h3>
             </div>
             <div className={styles.l_dir}>
               <button className={styles.filtro_b}>
                 <div>Filtros</div>
-                <div><Image src={filter} alt="fil"></Image></div>
+                <div>
+                  <Image src={filter} alt="fil"></Image>
+                </div>
               </button>
-              <div className={styles.filtro}>
-              </div>
+              <div className={styles.filtro}></div>
               <div className={styles.tags}>
                 <h3>tag 1</h3>
                 <h3>tag 2</h3>
@@ -66,22 +76,35 @@ export default function Produtos() {
             {prodsFilter.map((prod, key) => (
               <div key={key} className={styles.card_produtos}>
                 <div className={styles.card_img}>
-                  <Image src={prod.image[0]} className={styles.card_img} alt="prod" width={165} height={165}></Image>
+                  <Image
+                    src={prod.image[0]}
+                    className={styles.card_img}
+                    alt="prod"
+                    width={165}
+                    height={165}
+                  ></Image>
                 </div>
                 <div className={styles.informacoes}>
                   <h2>{prod.name}</h2>
                   <div className={styles.precos}>
-                    <div className={styles.current_price}>R$ {prod.current_price}</div>
+                    <div className={styles.current_price}>
+                      R$ {prod.current_price}
+                    </div>
                     <div className={styles.old_price}>R$ {prod.old_price}</div>
                   </div>
-                  <p className={styles.disponivel}>{prod.available_quantity} itens em estoque</p>
+                  <p className={styles.disponivel}>
+                    {prod.available_quantity} itens em estoque
+                  </p>
                 </div>
               </div>
             ))}
           </div>
         </div>
         <div className={styles.editar}>
-          <Link href="/edicao" style={{ textDecoration: 'none', color: 'white' }}>
+          <Link
+            href="/edicao"
+            style={{ textDecoration: 'none', color: 'white' }}
+          >
             <div>Editar</div>
           </Link>
         </div>
