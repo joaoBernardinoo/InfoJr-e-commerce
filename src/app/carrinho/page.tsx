904beb458af9323eadffe241d41b44a45d1cdb85
Lucas Júlio de Souza <lucasjuliosouza.06@gmail.com>
2023-12-09 07:16:12 -0300

    limpar carrinho

--- src/app/carrinho/page.tsx
+++ src/app/carrinho/page.tsx
@@ -14,7 +14,6 @@ import ProductContext from '@/contexts/products';
 import { ProductImage } from '@/enums/product_images';
 import { useContext } from 'react';
 
-import { addCart, removeCart } from '@/utils/functions';
 
 
 const produto = [
@@ -101,13 +100,14 @@ export default function Carrinho() {
     const newCart = cart.filter((prod) => prod.id !== id);
     setCart(newCart);
   }
-  // function updateProducts() {
-  //   if (products.length > 0) return;
-  //   for (let i = 0; i < produto.length; i++) {
-  //     products.push(produto[i]);
-  //   }
-  // }
-  // updateProducts();
+
+  const totalValue = cart.reduce((accumulator, product) => {
+    return accumulator + product.current_price;
+  }, 0);
+
+  function clearCart(){
+    setCart([]);
+  }
 
   return (
     <main>
@@ -205,7 +205,7 @@ export default function Carrinho() {
               <div className={style.totalEFrete}>
                 <div className={style.valorCompra}>
                   <div className={style.subtotal}>Subtotal</div>
-                  <div className={style.valorSubtotal}>R$ {products[0].current_price},00</div>
+                  <div className={style.valorSubtotal}>R$ {totalValue},00</div>
                 </div>            
                 <div className={style.botaoComprar}>
                   <button className={style.comprarAgora}>Comprar Agora</button>
@@ -223,7 +223,7 @@ export default function Carrinho() {
               </div>
               <div className={style.botoesCarrinho}>
                 <button className={style.botaoContinuar}>Continuar Comprando</button>
-                <button className={style.botaoLimpar}>Limpar Carrinho</button>
+                <button onClick={() => clearCart()} className={style.botaoLimpar}>Limpar Carrinho</button>
               </div>
 
             </div>
