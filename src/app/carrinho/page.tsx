76ea2d61d7c06f3f3cd5aa343dc7e8d8758a36eb
Lucas Júlio de Souza <lucasjuliosouza.06@gmail.com>
2023-12-09 11:04:28 -0300

    add cart, não atualiza

--- src/app/carrinho/page.tsx
+++ src/app/carrinho/page.tsx
@@ -10,10 +10,10 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import ProductContext from '@/contexts/products';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { ProductImage } from '@/enums/product_images';
 import { Product } from '@/types/products';
-import { otherProducts } from '@/utils/functions';
+import { otherProducts, addCart } from '@/utils/functions';
 
 const produto = [
   {
@@ -92,8 +92,7 @@ const produto = [
 // Ao colocar qualquer CEP, retorna o mesmo valor de frete ( ex: R$20,00)
 export default function Carrinho() {
   const { products, cart, setCart, recent } = useContext(ProductContext);
-  if (cart.length == 0) setCart(produto);
-
+  
   function deleteCart(id: number) {
     const newCart = cart.filter(prod => prod.id !== id);
     setCart(newCart);
@@ -114,6 +113,12 @@ export default function Carrinho() {
   function vendido(){
     alert('Compra finalizada!');
     clearCart();
+  }
+
+  function adiCart(prod: Product) {
+    const newCart = cart;
+    addCart(newCart, prod)
+    setCart(newCart);
   }
 
   const frete = 20
@@ -223,8 +228,7 @@ export default function Carrinho() {
                     </button>
                   </div>
                   <p className={style.msgRodape}>
-                    Preço de frete para Salvador, Bahia: R${' '}
-                    {products[0].current_price}
+                    Preço de frete para Salvador, Bahia: R${frete}
                   </p>
                 </div>
               </div>
@@ -248,8 +252,8 @@ export default function Carrinho() {
           <h1>Vistos Recentemente</h1>
         </div>
         <div className={style.recentesCards}>
-          {cart.map((prod, key) => (
-            <div className={style.cardContainer}>
+          {recent.map((prod, key) => (
+            <div key={key} className={style.cardContainer}>
               <Link
                 onClick={() => {
                   handleProduct(prod);
@@ -286,7 +290,7 @@ export default function Carrinho() {
                 {/* Adicionar ao carrinho */}
                 <button
                   className={style.btnAdicionar}
-                  onClick={() => (console.log('Deu certo'))}
+                  onClick={() => adiCart(prod)}
                   style={{ textDecoration: 'none', color: 'white' }}
                 >
                   Adicionar ao Carrinho
@@ -301,7 +305,7 @@ export default function Carrinho() {
           <h1>Outros Produtos</h1>
         </div>
         <div className={style.outrosProdsCards}>
-          {cart.map((prod, key) => (
+          {otherProducts(products).map((prod, key) => (
             <Link
               onClick={() => {
                 handleProduct(prod);
